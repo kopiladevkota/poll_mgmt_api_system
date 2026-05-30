ebd95c348cf3882f4414a43da95fc3f6f0f20a3b
kopiladevkota <kopiladevkota7@gmail.com>
2026-05-30 21:22:34 +0545

    Fix template paths and API URLs for production

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+# Create a superuser using environment variables (Add this to your build.sh)
+if [[ $CREATE_SUPERUSER ]];
+then
+    python manage.py createsuperuser --no-input
+    echo "Superuser creation attempted."
+fi
